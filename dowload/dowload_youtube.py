1ab1961b82bdc114dd01f1d2c88e9742be00656d
Daisel10 <davidsanchezlombana10@gmail.com>
2023-12-10 12:12:28 -0500

    ✅

--- dowload/dowload_youtube.py
+++ dowload/dowload_youtube.py
@@ -1,15 +1,18 @@
 from pytube import YouTube
 
 # URL del video de YouTube que deseas descargar
-url = 'https://www.youtube.com/shorts/aciqqV8JBRQ'
+url = ['https://www.youtube.com/shorts/aciqqV8JBRQ', 'https://www.youtube.com/shorts/dhUyLXM_Nr4']
 
-# Crea una instancia de la clase YouTube
-yt = YouTube(url)
+for i in url:
+    d = 'https://www.youtube.com/shorts/dhUyLXM_Nr4'
+    
+    print(i)
+    yt = YouTube(d)
 
-# Selecciona la mejor calidad disponible para la descarga
-video = yt.streams.get_highest_resolution()
+    # Selecciona la mejor calidad disponible para la descarga
+    video = yt.streams.get_highest_resolution()
 
-# Descarga el video en la carpeta actual (puedes especificar una ubicación personalizada)
-video.download()
+    # Descarga el video en la carpeta actual (puedes especificar una ubicación personalizada)
+    video.download("../videos/")
 
-print("Video descargado con éxito")
+    print("Video descargado con éxito")
